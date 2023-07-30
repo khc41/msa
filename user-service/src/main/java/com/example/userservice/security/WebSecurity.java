5b3fa8a21a5e1175e9a3cbfb81ddb28d29f7d2aa
heechul <khc4190@naver.com>
2023-07-30 15:20:29 +0900

    security 설정

--- user-service/src/main/java/com/example/userservice/security/WebSecurity.java
+++ user-service/src/main/java/com/example/userservice/security/WebSecurity.java
@@ -1,13 +1,29 @@
 package com.example.userservice.security;
 
+import com.example.userservice.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurity extends WebSecurityConfigurerAdapter {
+    private UserService userService;
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+    private Environment env;
+
+    @Autowired
+    public WebSecurity(Environment env, UserService userService, BCryptPasswordEncoder bCryptPasswordEncoder) {
+        this.env = env;
+        this.userService = userService;
+        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable();
@@ -19,6 +35,13 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
                 .addFilter(getAuthenticationFilter());
 
         http.headers().frameOptions().disable();
+    }
+
+    // select pwd from users where email=?
+    // db_pwd(encrypted) == input_pwd(encrypted)
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userService).passwordEncoder(bCryptPasswordEncoder);
     }
 
     private AuthenticationFilter getAuthenticationFilter() throws Exception {
