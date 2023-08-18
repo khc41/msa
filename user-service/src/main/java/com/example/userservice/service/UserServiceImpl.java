a6a9c1c7e7e88b5c53113aa8e2e2defe0061db3a
heechul <khc4190@naver.com>
2023-08-18 20:36:55 +0900

    feign client 예외처리

--- user-service/src/main/java/com/example/userservice/service/UserServiceImpl.java
+++ user-service/src/main/java/com/example/userservice/service/UserServiceImpl.java
@@ -5,6 +5,8 @@ import com.example.userservice.dto.UserDto;
 import com.example.userservice.jpa.UserEntity;
 import com.example.userservice.jpa.UserRepository;
 import com.example.userservice.vo.ResponseOrder;
+import feign.FeignException;
+import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,6 +23,7 @@ import java.util.List;
 import java.util.UUID;
 
 @Service
+@Slf4j
 public class UserServiceImpl implements UserService {
     UserRepository userRepository;
     BCryptPasswordEncoder passwordEncoder;
@@ -71,7 +74,13 @@ public class UserServiceImpl implements UserService {
 //                        new ParameterizedTypeReference<List<ResponseOrder>>() {
 //                        });
 //        List<ResponseOrder> orderList = orderListResponse.getBody();
-        List<ResponseOrder> orderList = orderServiceClient.getOrders(userId);
+        /* Feign Exception Handling */
+        List<ResponseOrder> orderList = null;
+        try {
+            orderList = orderServiceClient.getOrders(userId);
+        } catch (FeignException ex) {
+            log.error(ex.getMessage());
+        }
         userDto.setOrders(orderList);
 
         return userDto;
